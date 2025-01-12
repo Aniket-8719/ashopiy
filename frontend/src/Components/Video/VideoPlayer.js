93f169d4c17d9e2995c0f6b4b1bf18d898697567
Aniket Savita <21BCS8719@cuchd.in>
2025-01-12 23:01:28 +0530

    video library making

--- frontend/src/Components/Video/VideoPlayer.js
+++ frontend/src/Components/Video/VideoPlayer.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import video1 from "./WhatsApp Video 2025-01-12 at 21.51.22_f3c00a51.mp4";
+import { FaVideo } from "react-icons/fa";
 
 const videos = [
   {
@@ -13,7 +14,55 @@ const videos = [
     id: 2,
     title: "Login",
     description: "How to login, Forgot Password, Reset Password",
-    url: "https://www.w3schools.com/html/movie.mp4",
+    url: "#",
+  },
+  {
+    id: 3,
+    title: "Profile",
+    description: "How to Update Password, Edit Profile, Download full data",
+    url: "#",
+  },
+  {
+    id: 4,
+    title: "Subscription",
+    description: "How to buy subscription",
+    url: "#",
+  },
+  {
+    id: 5,
+    title: "Earning",
+    description: "How to login, Forgot Password, Reset Password",
+    url: "#",
+  },
+  {
+    id: 6,
+    title: "Charts",
+    description: "How to Download Per day, monthly & yearly data",
+    url: "#",
+  },
+  {
+    id: 7,
+    title: "Investment",
+    description: "How to add, update & delete investment",
+    url: "#",
+  },
+  {
+    id: 8,
+    title: "Udhaar Book",
+    description: "How to add, update and delete udhaar",
+    url: "#",
+  },
+  {
+    id: 9,
+    title: "History",
+    description: "How to see your total cash, online cash, Return customers, Return amount, Total active days",
+    url: "#",
+  },
+  {
+    id: 10,
+    title: "App Lock",
+    description: "How to lock your features",
+    url: "#",
   },
   // Add more video objects here
 ];
@@ -33,7 +82,16 @@ const VideoPlayer = () => {
                 className="bg-white shadow-lg rounded-lg p-4 hover:shadow-xl transition-shadow cursor-pointer"
                 onClick={() => setCurrentVideo(video)}
               >
-                <h2 className="text-xl font-semibold mb-2">{video.title}</h2>
+                <div className="flex justify-between items-center">
+                  <div>
+                    <h2 className="text-xl font-semibold mb-2">
+                      {video.title}
+                    </h2>
+                  </div>
+                  <div className="mr-4">
+                    <FaVideo />
+                  </div>
+                </div>
                 <p className="text-gray-700 mb-4">{video.description}</p>
               </div>
             ))}
@@ -42,7 +100,7 @@ const VideoPlayer = () => {
           {/* Video Popup */}
           {currentVideo && (
             <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 ">
-              <div className="w-full max-w-md max-h-[600px] h-full p-6 bg-white rounded-lg shadow-lg relative mx-2">
+              <div className="w-full max-w-md max-h-[600px] h-full p-8 bg-white rounded-lg shadow-lg relative mx-2">
                 <button
                   onClick={() => setCurrentVideo(null)}
                   className="absolute top-2 right-2 w-8 h-8 flex items-center justify-center bg-gray-300 text-gray-600 rounded-full hover:bg-gray-400 hover:text-white"
